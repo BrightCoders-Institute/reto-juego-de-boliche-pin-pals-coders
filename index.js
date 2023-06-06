ef94e954e74090fc7c7de56d1d388aa8a2a3cc98
Abdielsantillan <santillanabdiel0@gmail.com>
2023-06-06 17:38:58 -0600

    implementation of the conditions
    In the implementation, 4 conditions were added, which change depending on the iteration of the cycle index, the first iteration must have the random value of 10 since there are 10 pins, the second iteration depends on whether a strike was made In the third condition, depending on whether there was a spare or strike, a new roll is added, and the last iteration is the sum of the pins thrown
    
    Co-authored-by: FebernalGober <alexis.ferber18@gmail.com>
    Co-authored-by: Axw3ll <axelomar.a.v@gmail.com>

--- index.js
+++ index.js
@@ -4,40 +4,47 @@ function shoot(n){
     return pine
 }
 var arrayPrincipal = []
-function implementacion(){
+  function implementacion(){
     for(var i = 0; i<10; i++){
         var arraysecunadrio =[]
         for(var j =0; j<4 ; j++){
-            if(j == 0){
-                tiro = shoot()//5 
-                arraysecunadrio.push(tiro)
-            }else if(j == 1){
-                if(arraysecunadrio[0]<10){
-                    tiro = shoot(10-arraysecunadrio[0])   
+            if(j == 0){ //Se  hace el primer tiro 
+                tiro = shoot(10) //Como es el primer tiro todos lo pinos estan arriba
+                arraysecunadrio.push(tiro)  //Se agregar el valor al array
+            }else if(j == 1){ //Se hace el segundo tiro
+                if(arraysecunadrio[0]<10){//si no se hace un strike se hace el siguiente tiro
+                    tiro = shoot(10-arraysecunadrio[0])   //1
                     arraysecunadrio.push(tiro)
                    }
-            }
-            else if ( j == 2){
-                if(IsSpare(arraysecunadrio[0],arraysecunadrio[1])){
+                else{//Si se hace strike se reinician los bolos
                     tiro = shoot(10)
+                    arraysecunadrio.push(tiro)
                 }
-                else{
+            }
+            else if ( j == 2){//Cuando esta en la terscer tirada
+                if(IsSpare(arraysecunadrio[0],arraysecunadrio[1]) || arraysecunadrio[0]==10){ //si se hace un spare entonces se hace el bonus 
+                    tiro = shoot(10)
+                    arraysecunadrio.push(tiro)
+                }
+                else{//en el caso de que no sea chusa o ni strike se tiene que agregar un 0
                     arraysecunadrio.push(0)
                 }
             }else if(j == 3 ){
-                totalTiros = sumaTiros(arraysecunadrio[0],arraysecunadrio[1],arraysecunadrio[2])
-                arraysecunadrio.push(totalTiros)
+                if(i == 0){
+                    totalTiros = sumaTiros(arraysecunadrio[0],arraysecunadrio[1],arraysecunadrio[2])
+                    arraysecunadrio.push(totalTiros)
+                }else{
+                    totalTiros = sumaTiros(arraysecunadrio[0],arraysecunadrio[1],arraysecunadrio[2])
+                    total = sumaTirosAnteriores(totalTiros , arrayPrincipal[i-1][3])
+                    arraysecunadrio.push(total)
+  
+                }
             }
         }
         arrayPrincipal.push(arraysecunadrio)
     }
     console.log(arrayPrincipal)
-}
-
-var arrayPrincipal = []
-
-
-
+  }
 
 function IsSpare(shoot1,shoot2){
     if((shoot1+shoot2)==10){
@@ -56,3 +63,4 @@ function IsSpare(shoot1,shoot2){
     return total
 }
   
+implementacion()
